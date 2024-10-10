1bebecec9f04fa387e81a9ebeab628d1a850429f
Tyler Smart <tylerstevensmart@gmail.com>
2024-10-10 14:50:43 -0600

    Implement LengthOfLongestSubstring.

---
+++ C#/LeetCode/LengthOfLongestSubstring.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.LengthOfLongestSubstring
+{
+    public static class Solution
+    {
+        public static int Solve(string s)
+        {
+            int longest = 0;
+            HashSet<char> charSet = new HashSet<char>();
+            Queue<char> charQueue = new Queue<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine($"Processing '{s[i]}'");
+                if (charSet.Contains(s[i]))
+                {
+                    if (charSet.Count > longest)
+                    {
+                        longest = charSet.Count;
+                    }
+
+                    charSet.Remove(s[i]);
+                    while (charQueue.Count > 0)
+                    {
+                        char popped = charQueue.Dequeue();
+                        if (popped == s[i]) break;
+                        charSet.Remove(popped);
+                    }
+                }
+                charSet.Add(s[i]);
+                charQueue.Enqueue(s[i]);
+            }
+            if (charSet.Count > longest)
+            {
+                longest = charSet.Count;
+            }
+
+            return longest;
+        }
+    }
+}
--- C#/Tests/LeetCodeTests.cs
+++ C#/Tests/LeetCodeTests.cs
@@ -9,8 +9,8 @@ namespace Tests
         [TestMethod]
         public void MaximumWidthRamp()
         {
-            Assert.AreEqual(LeetCode.MaximumWidthRamp.Solution.Solve([6, 0, 8, 2, 1, 5]), 4);
-            Assert.AreEqual(LeetCode.MaximumWidthRamp.Solution.Solve([9, 8, 1, 0, 1, 9, 4, 0, 4, 1]), 7);
+            Assert.AreEqual(4, LeetCode.MaximumWidthRamp.Solution.Solve([6, 0, 8, 2, 1, 5]));
+            Assert.AreEqual(7, LeetCode.MaximumWidthRamp.Solution.Solve([9, 8, 1, 0, 1, 9, 4, 0, 4, 1]));
         }
 
         [TestMethod]
@@ -27,6 +27,15 @@ namespace Tests
             Assert.IsTrue(LeetCode.AddTwoNumbers.Solution.Solve(LeetCode.AddTwoNumbers.ListNode.FromArray([2, 4, 3]), LeetCode.AddTwoNumbers.ListNode.FromArray([5, 6, 4])).ToArray().SequenceEqual([7, 0, 8]));
             Assert.IsTrue(LeetCode.AddTwoNumbers.Solution.Solve(LeetCode.AddTwoNumbers.ListNode.FromArray([0]), LeetCode.AddTwoNumbers.ListNode.FromArray([0])).ToArray().SequenceEqual([0]));
             Assert.IsTrue(LeetCode.AddTwoNumbers.Solution.Solve(LeetCode.AddTwoNumbers.ListNode.FromArray([9, 9, 9, 9, 9, 9, 9]), LeetCode.AddTwoNumbers.ListNode.FromArray([9, 9, 9, 9])).ToArray().SequenceEqual([8, 9, 9, 9, 0, 0, 0, 1]));
+        }
+
+        [TestMethod]
+        public void LengthOfLongestSubstring()
+        {
+            Assert.AreEqual(3, LeetCode.LengthOfLongestSubstring.Solution.Solve("abcabcbb"));
+            Assert.AreEqual(1, LeetCode.LengthOfLongestSubstring.Solution.Solve("bbbbb"));
+            Assert.AreEqual(3, LeetCode.LengthOfLongestSubstring.Solution.Solve("pwwkew"));
+            Assert.AreEqual(1, LeetCode.LengthOfLongestSubstring.Solution.Solve(" "));
         }
     }
 }
