6a6d1a488885e724ec5bcba9d84638d0f976acc7
Tyler Smart <tylerstevensmart@gmail.com>
2024-10-11 13:11:44 -0600

    Improve LongestPalindromicSubstring

--- C#/LeetCode/LongestPalindromicSubstring.cs
+++ C#/LeetCode/LongestPalindromicSubstring.cs
@@ -10,36 +10,39 @@ namespace LeetCode.LongestPalindromicSubstring
     {
         public static string Solve(string s)
         {
-            // BRUTE FORCE APPROACH
-
-            if (s.Length <= 1) return s;
-
             string longest = "";
 
             for (int i = 0; i < s.Length; i++)
             {
-                for (int j = 0; j < s.Length - i; j++)
+                int len1 = ExpandAroundCenter(s, i, i);
+                int len2 = ExpandAroundCenter(s, i, i + 1);
+                int maxLen = Math.Max(len1, len2);
+
+                int start = i - (maxLen - 1) / 2;
+                int end = i + maxLen / 2;
+                string current = s.Substring(start, end - start + 1);
+
+                if (current.Length > longest.Length)
                 {
-                    string current = s.Substring(i, j + 1);
-
-                    bool isPalindrome = true;
-                    for (int k = 0; k < current.Length / 2; k++)
-                    {
-                        if (current[k] != current[current.Length - k - 1])
-                        {
-                            isPalindrome = false;
-                            break;
-                        }
-                    }
-                    
-                    if (isPalindrome && current.Length > longest.Length) longest = current;
+                    longest = current;
                 }
-                
-
             }
 
             return longest;
+        }
 
+        private static int ExpandAroundCenter(string s, int left, int right)
+        {
+            if (string.IsNullOrEmpty(s) || left > right)
+                return 0;
+
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+
+            return right - left - 1;
         }
     }
 }
