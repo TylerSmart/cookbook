25155a85026d5c824bed7c56518585c67938905c
Tyler Smart <tylerstevensmart@gmail.com>
2024-10-14 12:42:48 -0600

    Implement Can Place Flowers

---
+++ C#/LeetCode/CanPlaceFlowers.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.CanPlaceFlowers
+{
+    public static class Solution
+    {
+        public static bool Solve(int[] flowerbed, int n)
+        {
+            if (n == 0) return true;
+            if (n > flowerbed.Length) return false;
+
+            for (int i = 0; i < flowerbed.Length; i++)
+            {
+                if (flowerbed[i] == 0 && (i - 1 < 0 || flowerbed[i - 1] == 0) && (i + 1 >= flowerbed.Length || flowerbed[i + 1] == 0))
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                    if (n == 0) return true;
+                }
+            }
+            return false;
+        }
+    }
+}
