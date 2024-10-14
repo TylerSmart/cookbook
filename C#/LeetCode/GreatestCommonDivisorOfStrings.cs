855b55a360a99084faca1c656177db21cc56da1e
Tyler Smart <tylerstevensmart@gmail.com>
2024-10-14 12:08:46 -0600

    Improved Greatest Common Divisor of Strings

--- C#/LeetCode/GreatestCommonDivisorOfStrings.cs
+++ C#/LeetCode/GreatestCommonDivisorOfStrings.cs
@@ -11,29 +11,22 @@ namespace LeetCode.GreatestCommonDivisorOfStrings
     {
         public static string Solve(string str1, string str2)
         {
-            bool IsValid(string str1, string str2, int k)
-            {
-                int len1 = str1.Length, len2 = str2.Length;
-                if (len1 % k > 0 || len2 % k > 0)
-                {
-                    return false;
-                }
-                else
-                {
-                    string baseStr = str1.Substring(0, k);
-                    return string.Concat(Enumerable.Repeat(baseStr, len1 / k)) == str1 && string.Concat(Enumerable.Repeat(baseStr, len2 / k)) == str2;
-                }
-            }
+            if (str1 + str2 != str2 + str1) return "";
 
-            int len1 = str1.Length, len2 = str2.Length;
-            for (int i = Math.Min(len1, len2); i >= 1; i--)
+            int gcdLength = GCD(str1.Length, str2.Length);
+
+            return str1.Substring(0, gcdLength);
+        }
+
+        public static int GCD(int a, int b)
+        {
+            while (b != 0)
             {
-                if (IsValid(str1, str2, i))
-                {
-                    return str1.Substring(0, i);
-                }
+                int temp = b;
+                b = a % b;
+                a = temp;
             }
-            return "";
+            return a;
         }
     }
 }
