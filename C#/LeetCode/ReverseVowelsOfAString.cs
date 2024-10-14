f99d555712561933c81068b7b16c02fec89d75e0
Tyler Smart <tylerstevensmart@gmail.com>
2024-10-14 12:52:17 -0600

    Implement ReverseVowelsOfAString

---
+++ C#/LeetCode/ReverseVowelsOfAString.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.ReverseVowelsOfAString
+{
+    public static class Solution
+    {
+        public static string Solve(string s)
+        {
+            char[] VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+            char[] sArray = s.ToCharArray();
+
+            for (int i = 0, j = sArray.Length - 1; i < j && j > i;)
+            {
+                if (VOWELS.Contains(sArray[i]) == false)
+                {
+                    i++;
+                }
+
+                if (VOWELS.Contains(sArray[j]) == false)
+                {
+                    j--;
+                }
+
+                if (VOWELS.Contains(s[i]) && VOWELS.Contains(s[j]))
+                {
+                    char temp = sArray[i];
+                    sArray[i] = sArray[j];
+                    sArray[j] = temp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            return string.Join("", sArray);
+        }
+    }
+}
